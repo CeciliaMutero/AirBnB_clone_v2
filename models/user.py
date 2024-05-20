205e3437c6e22b4c214752e76838ca008397cf22
Cecilia <cecilmutero66@gmail.com>
2024-05-20 01:59:23 -0700

    7. DBStorage - User

--- models/user.py
+++ models/user.py
@@ -1,11 +1,21 @@
 #!/usr/bin/python3
 """This module defines a class User"""
 from models.base_model import BaseModel
+from sqlalchemy import Column, String, create_engine, DateTime
+from sqlalchemy.orm import sessionmaker
+from models.base_model import BaseModel, Base
 
 
-class User(BaseModel):
+
+class User(BaseModel, Base):
     """This class defines a user by various attributes"""
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+    __tablename__ = 'users'
+    email = Column(String(128), nullable=True)
+    password = Column(String(128), nullable=True)
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
+
+engine = create_engine('mysql://hbnb_dev:hbnb_dev_pwd@localhost/hbnb_dev_db')
+Base.metadata.create_all(engine)
+Session = sessionmaker(bind=engine)
+session = Session()
