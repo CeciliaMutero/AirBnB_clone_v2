1948970e50995d381b190f411df41c337299ac35
Cecilia <cecilmutero66@gmail.com>
2024-06-27 00:20:36 -0700

    3. Python is cool!

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -25,7 +25,7 @@ def c_text(text):
     return f"C {text}"
 
 
-@app.route('/python/', defaults={'text' : 'is cool'}, strict_slashes=False)
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def c_python(text):
     """returns “Python ” followed by the value of the text variable"""
